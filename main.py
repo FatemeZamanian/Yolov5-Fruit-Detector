a4e8e26ca069d212bc108bf3ed7da51affa1129e
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2021-09-03 14:38:23 +0430

    show img

--- main.py
+++ main.py
@@ -4,6 +4,44 @@ import os
 from PySide6.QtWidgets import QApplication, QWidget,QFileDialog
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
+from PyQt5.QtGui import QImage
+from PySide6.QtGui import QPixmap
+import cv2
+from PySide6.QtCore import QThread, Signal, QDir
+
+
+def convertCVImage2QtImage(cv_img):
+    cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+    height, width, channel = cv_img.shape
+    bytesPerLine = 3 * width
+    qimg = QImage(cv_img.data, width, height, bytesPerLine, QImage.Format_RGB888)
+    return QPixmap.fromImage(qimg)
+
+
+class show_video(QThread):
+    signal_show_frame = Signal(object)
+
+    def __init__(self):
+        super(show_video, self).__init__()
+
+    def run(self):
+        self.video = cv2.VideoCapture(0)
+
+        while True:
+            valid, self.frame = self.video.read()
+            if valid is not True:
+                break
+            self.signal_show_frame.emit(self.frame)
+            cv2.waitKey(30)
+
+    def stop(self):
+        try:
+            self.video.release()
+        except:
+            pass
+
+
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -17,8 +55,10 @@ class MainWindow(QWidget):
 
     def getfile(self):
         self.fileName = QFileDialog.getOpenFileName(self,'Single File','C:\'','*.jpg *.mp4 *.jpeg *.png *.avi')
-        self.file = str(self.fileName[0])
-        self.ui.txt_address.setText(self.file)
+        self.file = self.fileName[0]
+        self.ui.txt_address.setText(str(self.file))
+        
+        self.ui.lbl_out.setPixmap(self.file)
         return self.file
         
     def predict(self):
