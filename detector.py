2a6c8c18f978634d3605ec4b859b3697fe1b640d
Fateme Zamanian <fatemehzamanian.1999@gmail.com>
2021-09-08 03:51:38 +0430

    weight edited

--- detector.py
+++ detector.py
@@ -15,7 +15,7 @@ from utils.torch_utils import select_device
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--weights', nargs='+', type=str, default='weights/yolov5s.pt', help='model.pt path(s)')
+parser.add_argument('--weights', nargs='+', type=str, default='weights/Fruits.pt', help='model.pt path(s)')
 parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
 parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
 parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
